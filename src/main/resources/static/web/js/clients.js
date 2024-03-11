2c350b3dce8842b571b08b9c33ddfd3017ac4b34
EliasPuddini <131315510+EliasPuddini1@users.noreply.github.com>
2024-03-10 21:05:37 -0300

    Primer avance

--- src/main/resources/static/web/js/clients.js
+++ src/main/resources/static/web/js/clients.js
@@ -1,4 +1,39 @@
-Vue.createApp({
+windows.addEventListener('load',() =>{
+
+    const app = vue.createApp({
+        data() {
+            return {
+                clientInfo: {},
+                errorToats: null,
+                errorMsg: null,
+            }
+        },
+        methods: {
+
+            
+
+            getData: function () {
+                axios.get("/api/clients/")
+                    .then((response) => {
+                        //get client ifo
+                        this.clientInfo = response.data;
+                    })
+                    .catch((error) => {
+                        // handle error
+                        this.errorMsg = "Error getting data";
+                        this.errorToats.show();
+                    })
+            },
+            formatDate: function (date) {
+                return new Date(date).toLocaleDateString('en-gb');
+            }
+        }     
+
+    })
+    app.mount('#app')
+});
+
+/*Vue.createApp({
     data() {
         return {
             clientInfo: {},
@@ -28,4 +63,4 @@ Vue.createApp({
         this.getData();
         new DataTable('#example');
     }
-}).mount('#app')
+}).mount('#app')*/
