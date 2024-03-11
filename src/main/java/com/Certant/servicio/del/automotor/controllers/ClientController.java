c13e977c78bc851586ea5ab2be9fd1e8c22d3232
EliasPuddini1 <97805053+EliasPuddini@users.noreply.github.com>
2024-03-10 23:25:57 -0300

    Callejón sin salida
    Se intento obtener la lista de clientes de la api, /api/clients, lo cual no resulto. Tras varios intentos se decidió no crear el front end y dejar el archivo en su estado actual hasta tener mas información y poder resolver el problema. Las pruebas con postman mostraron que la api funciona correctamente.

--- src/main/java/com/Certant/servicio/del/automotor/controllers/ClientController.java
+++ src/main/java/com/Certant/servicio/del/automotor/controllers/ClientController.java
@@ -11,7 +11,7 @@ import java.util.List;
 import java.util.Optional;
 
 @RestController
-@RequestMapping(path = "/api/clients")
+@RequestMapping("/api/clients")
 public class ClientController {
 
     @Autowired
--- src/main/resources/static/web/js/clients.js
+++ src/main/resources/static/web/js/clients.js
@@ -1,66 +1,39 @@
-windows.addEventListener('load',() =>{
+window.addEventListener('load',() =>{
 
-    const app = vue.createApp({
+    const app = Vue.createApp({
         data() {
             return {
-                clientInfo: {},
+                clients: [],
+                client:{
+                    id: '',
+                    nombre: '',
+                    tipoCliente: '',
+                    serviciosPrevios: ''
+                },
                 errorToats: null,
                 errorMsg: null,
             }
         },
         methods: {
-
-            
-
-            getData: function () {
-                axios.get("/api/clients/")
-                    .then((response) => {
-                        //get client ifo
-                        this.clientInfo = response.data;
-                    })
-                    .catch((error) => {
-                        // handle error
-                        this.errorMsg = "Error getting data";
-                        this.errorToats.show();
-                    })
+            getData: async function () {
+                try {
+                    const response = await fetch('/api/clients');
+                    const data = await response.data;
+                    this.clients = data;
+                    console.log(this.clients);
+                } catch (error) {
+                    this.errorMsg = "Error getting data";
+                    this.errorToats.show();
+                }
             },
             formatDate: function (date) {
                 return new Date(date).toLocaleDateString('en-gb');
             }
-        }     
+        },
+        mounted: function(){
+            this.getData();
+        }
 
     })
     app.mount('#app')
 });
-
-/*Vue.createApp({
-    data() {
-        return {
-            clientInfo: {},
-            errorToats: null,
-            errorMsg: null,
-        }
-    },
-    methods: {
-        getData: function () {
-            axios.get("/api/clients/")
-                .then((response) => {
-                    //get client ifo
-                    this.clientInfo = response.data;
-                })
-                .catch((error) => {
-                    // handle error
-                    this.errorMsg = "Error getting data";
-                    this.errorToats.show();
-                })
-        },
-        formatDate: function (date) {
-            return new Date(date).toLocaleDateString('en-gb');
-        }
-    },
-    mounted: function () {
-        this.errorToats = new bootstrap.Toast(document.getElementById('danger-toast'));
-        this.getData();
-        new DataTable('#example');
-    }
-}).mount('#app')*/
