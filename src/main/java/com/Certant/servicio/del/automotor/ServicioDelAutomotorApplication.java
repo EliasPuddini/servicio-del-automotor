21673833bbab90e410d8d88fd3691ee9cfe2082d
EliasPuddini1 <97805053+EliasPuddini@users.noreply.github.com>
2025-01-08 16:19:27 -0300

    Vuelta a empezar
     Debido a los nuevos conocimientos se ha encontrado que el diagrama de clases es erroneo.

--- src/main/java/com/Certant/servicio/del/automotor/ServicioDelAutomotorApplication.java
+++ src/main/java/com/Certant/servicio/del/automotor/ServicioDelAutomotorApplication.java
@@ -1,13 +1,7 @@
 package com.Certant.servicio.del.automotor;
 
-import com.Certant.servicio.del.automotor.repositories.ClientRepository;
-import com.Certant.servicio.del.automotor.repositories.ServicioRepository;
-import com.Certant.servicio.del.automotor.repositories.TurnoRepository;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
 
 
 @SpringBootApplication
